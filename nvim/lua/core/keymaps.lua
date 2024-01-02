f291d6c6481d4f024767158f4ecf9470734e5e3f
patnealcodes <hi@patneal.codes>
2024-01-02 10:51:03 -0500

    looking good

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -5,7 +5,13 @@ local keymap = vim.keymap
 keymap.set("c", "<Down>", "<C-n>", {})
 keymap.set("c", "<Up>", "<C-p>", {})
 
-keymap.set("x", "<C-R>", "y<Esc>:%s/<C-R>'//g<Left><Left>", {})
+keymap.set("x", "<leader>s", "y<Esc>:%s/<C-R>'//g<Left><Left>", {})
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+keymap.set("x", "p", [["_dP]])
+keymap.set({ "n", "v" }, "y", [["+y]])
+keymap.set("n", "Y", [["+Y]])
+keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
