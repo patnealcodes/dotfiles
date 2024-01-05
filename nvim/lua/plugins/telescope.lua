f0e49833f4b36e007fe7d89659e0f3c8e4266e0d
patnealcodes <hi@patneal.codes>
2024-01-05 15:04:31 -0500

    keymap stuff

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -42,7 +42,7 @@ return {
     vim.keymap.set('n', '<leader>ff', builtin.find_files)
     vim.keymap.set('n', '<leader>fo', builtin.oldfiles)
     vim.keymap.set('n', '<leader>fg', builtin.git_files)
-		vim.keymap.set("n", "<leader>ob", "<cmd>Telescope buffers<cr>")
+		vim.keymap.set("n", "<leader>bb", "<cmd>Telescope buffers<cr>")
     vim.keymap.set('n', '<leader>lg', builtin.live_grep)
     vim.keymap.set('n', '<leader>gs', function()
       builtin.grep_string({ search = vim.fn.input("gimee > ") })
--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,8 +1,19 @@
 return {
   "folke/trouble.nvim",
   config = function()
-    require("trouble").setup({
+    local trouble = require("trouble")
+    trouble.setup({
       icons = false
     })
+
+    vim.keymap.set('n', '<leader>tt', function ()
+      trouble.toggle()
+    end)
+    vim.keymap.set('n', '<leader>tn', function ()
+      trouble.next()
+    end)
+    vim.keymap.set('n', '<leader>tp', function ()
+      trouble.prev()
+    end)
   end
 }
