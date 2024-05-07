2d02767a614d0bb9bebcb583331d28fbe7ac23c3
patnealcodes <hi@patneal.codes>
2024-05-07 18:22:59 -0400

    ala

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,61 +1,61 @@
 return {
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	config = function()
-	-- 		require("tokyonight").setup({
-	-- 			style = "night",
-	-- 			transparent = true,
-	-- 			terminal_colors = true,
-	-- 			styles = {
-	-- 				comments = { italic = false },
-	-- 				keywords = { italic = false },
-	-- 			},
-	-- 			dim_inactive = true,
-	-- 		})
-	--
-	-- 		vim.cmd("colorscheme tokyonight")
-	-- 	end,
-	-- },
-
-	-- {
-	-- 	"rose-pine/neovim",
-	-- 	name = "rose-pine",
-	-- 	config = function()
-	-- 		require("rose-pine").setup({
-	-- 			disable_background = true,
-	-- 			styles = {
-	-- 				italic = false,
-	-- 			},
-	-- 		})
-	--
-	-- 		vim.cmd("colorscheme rose-pine")
-	-- 	end,
-	-- },
-
 	{
-		"AlexvZyl/nordic.nvim",
-		lazy = false,
-		priority = 1000,
+		"folke/tokyonight.nvim",
 		config = function()
-			local C = require("nordic.colors.nordic")
-			require("nordic").load({
-				italic_comments = false,
-				transparent_bg = true,
-				bright_border = true,
-				override = {
-					TreesitterContext = { bg = C.black0 },
+			require("tokyonight").setup({
+				style = "night",
+				transparent = true,
+				terminal_colors = true,
+				styles = {
+					comments = { italic = false },
+					keywords = { italic = false },
 				},
-				cursorline = {
-					bold = false,
-					bold_number = true,
-					theme = "light",
-				},
-				telescope = {
-					style = "classic",
-				},
+				dim_inactive = true,
 			})
+
+			-- vim.cmd("colorscheme tokyonight")
 		end,
 	},
+
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					italic = false,
+				},
+			})
+
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
+
+	-- {
+	-- 	"AlexvZyl/nordic.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		local C = require("nordic.colors.nordic")
+	-- 		require("nordic").load({
+	-- 			italic_comments = false,
+	-- 			transparent_bg = true,
+	-- 			bright_border = true,
+	-- 			override = {
+	-- 				TreesitterContext = { bg = C.black0 },
+	-- 			},
+	-- 			cursorline = {
+	-- 				bold = false,
+	-- 				bold_number = true,
+	-- 				theme = "light",
+	-- 			},
+	-- 			telescope = {
+	-- 				style = "classic",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 
 	-- {
 	-- 	"catppuccin/nvim",
