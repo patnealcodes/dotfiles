eac00b9480bfcfe66e110ab4265962156614e225
patnealcodes <hi@patneal.codes>
2024-09-13 13:08:40 -0400

    styles & lsp streamline

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,30 +1,3 @@
-local servers = {
-  "lua_ls",
-  "tsserver",
-  "cssls",          -- css-lsp
-  "gopls",
-  "html",           -- html-lsp
-  "jsonls",         -- json-lsp
-  "lua_ls",         -- lua-language-server
-  "markdown_oxide", -- markdown-oxide
-  "pyright",
-  "ruff_lsp",
-  "rust_analyzer",
-  "tsserver", -- typescript-language-server
-  "yamlls",   -- yaml-language-server
-}
-local formatters = {
-  "black",
-  "isort",
-  "prettier",
-  "prettierd",
-  "stylua",
-  "gofumpt",
-  "goimports_reviser",
-  "pyright",
-  "rustfmt",
-  "rustywind",
-}
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
@@ -36,91 +9,90 @@ return {
     "hrsh7th/cmp-cmdline",
     "hrsh7th/nvim-cmp",
     "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
     "j-hui/fidget.nvim",
   },
-  servers = servers,
-  formatters = formatters,
 
   config = function()
-    local cmp = require "cmp"
-    local cmp_lsp = require "cmp_nvim_lsp"
-    local capabilities =
-        vim.tbl_deep_extend(
-          "force",
-          {},
-          vim.lsp.protocol.make_client_capabilities(),
-          cmp_lsp.default_capabilities())
-    local ensure_installed = vim.tbl_extend("keep", servers, formatters)
-    local lspconfig = require "lspconfig"
+    local cmp = require('cmp')
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities())
 
-    capabilities.workspace = {
-      didChangeWatchedFiles = {
-        dynamicRegistration = true,
-      },
-    }
-
-    require("fidget").setup {}
+    require("fidget").setup({})
     require("mason").setup()
-    require("mason-lspconfig").setup {
-      ensure_installed = ensure_installed,
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "cssls",  -- css-lsp
+        "html",   -- html-lsp
+        "jsonls", -- json-lsp
+        "lua_ls", -- lua-language-server
+        "pyright",
+        "ruff_lsp",
+        "rust_analyzer",
+        "tsserver", -- typescript-language-server
+        "yamlls",   -- yaml-language-server
+      },
       handlers = {
         function(server_name)
           require("lspconfig")[server_name].setup {
-            capabilities = capabilities,
-          }
-        end,
-        ["rust_analyzer"] = function()
-          lspconfig.rust_analyzer.setup {
-            capabilities = capabilities,
-            settings = {
-              ["rust-analyzer"] = {
-                checkOnSave = {
-                  command = "clippy",
-                },
-              },
-            },
+            capabilities = capabilities
           }
         end,
 
         ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
           lspconfig.lua_ls.setup {
             capabilities = capabilities,
             settings = {
               Lua = {
                 runtime = { version = "Lua 5.1" },
                 diagnostics = {
-                  globals = { "vim" },
-                },
-              },
-            },
+                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                }
+              }
+            }
           }
         end,
-      },
-    }
+      }
+    })
 
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-    cmp.setup {
+    cmp.setup({
       snippet = {
         expand = function(args)
-          require("luasnip").lsp_expand(args.body)
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         end,
       },
-      mapping = cmp.mapping.preset.insert {
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-y>"] = cmp.mapping.confirm { select = true },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
-      },
-    }
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+      }, {
+        { name = 'buffer' },
+      })
+    })
 
-    vim.diagnostic.config {
+    vim.diagnostic.config({
+      -- update_in_insert = true,
       float = {
         focusable = false,
+        style = "minimal",
+        border = "rounded",
         source = "always",
-        -- header = "",
-        -- prefix = "",
+        header = "",
+        prefix = "",
       },
-    }
-  end,
+    })
+  end
 }
