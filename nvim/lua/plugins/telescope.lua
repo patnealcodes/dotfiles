82b8e3ae3d8f6c9da0fbecb883869661295b3d85
patnealcodes <hi@patneal.codes>
2024-09-20 18:32:49 -0400

    simplify telescope & treesitter config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,115 +1,64 @@
 return {
-	"nvim-telescope/telescope-ui-select.nvim",
-	{
-		"nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-		tag = "0.1.5",
+	tag = "0.1.5",
 
-		dependencies = { "nvim-lua/plenary.nvim", },
-
-		config = function()
-			local function filenameFirst(_, path)
-				local tail = vim.fs.basename(path)
-				local parent = vim.fs.dirname(path)
-				if parent == "." then
-					return tail
-				end
-				return string.format("%s\t\t%s", tail, parent)
-			end
-
-			local previewers = require("telescope.previewers")
-			local sorters = require("telescope.sorters")
-			require("telescope").load_extension("ui-select")
-			require("telescope").load_extension('harpoon')
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-					fzf = {
-						fuzzy = true, -- false will only do exact matching
-						override_generic_sorter = true, -- override the generic sorter
-						override_file_sorter = true, -- override the file sorter
-						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-					},
-				},
-				defaults = {
-					winblend = 0,
-					layout_strategy = "vertical",
-					dynamic_preview_title = true,
-					vimgrep_arguments = {
-						"rg",
-						"--color=never",
-						"--no-heading",
-						"--with-filename",
-						"--line-number",
-						"--column",
-						"--smart-case",
-						"--hidden",
-						"--glob=!.git/*",
-						"--glob=!**/node_modules/*",
-						"--glob=!**/package-lock.json",
-					},
-					path_display = filenameFirst,
-				},
-				pickers = {
-					live_grep = {
-						only_sort_text = true,
-					},
-					grep_string = {
-						only_sort_text = true,
-					},
-					buffers = {
-						mappings = {
-							i = {
-								["<C-x>"] = "delete_buffer",
-							},
-							n = {
-								["<C-x>"] = "delete_buffer",
-							},
-						},
-					},
-					git_files = {
-						hidden = true,
-						show_untracked = true,
-					},
-					colorscheme = {
-						enable_preview = true,
-					},
-				},
-				file_previewer = previewers.vim_buffer_cat.new,
-				grep_previewer = previewers.vim_buffer_vimgrep.new,
-				qflist_previewer = previewers.vim_buffer_qflist.new,
-				file_sorter = sorters.get_fuzzy_file,
-				generic_sorter = sorters.get_generic_fuzzy_sorter,
-			})
-			local builtin = require("telescope.builtin")
-
-			vim.keymap.set("n", "<leader>s?", builtin.help_tags, { desc = "[S]earch [H]elp" })
-			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", function() builtin.find_files() end, { desc = "[S]earch [F]iles" })
-			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-			vim.keymap.set("n", "<leader>sg", function()
-				builtin.live_grep({
-					sorting_strategy = "ascending",
-					layout_strategy = "bottom_pane",
-					prompt_prefix = ">> ",
-					prompt_title = "Live Grep",
-					hidden = false
-				})
-			end)
-			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-			vim.keymap.set("n", "<leader>sC", builtin.colorscheme, { desc = "[S]earch colorschemes" })
-			vim.keymap.set("n", "<leader>sH", builtin.highlights, { desc = "[S]earch highlights" })
-
-			vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-			vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
-			vim.keymap.set("n", "<leader>d", builtin.diagnostics, {})
-			vim.keymap.set("n", "gr", builtin.lsp_references, {})
-		end,
+	dependencies = {
+		"nvim-lua/plenary.nvim"
 	},
+
+	config = function()
+		local function filenameFirst(_, path)
+			local tail = vim.fs.basename(path)
+			local parent = vim.fs.dirname(path)
+			if parent == "." then
+				return tail
+			end
+			return string.format("%s\t\t%s", tail, parent)
+		end
+
+		require('telescope').setup({
+			defaults = {
+				winblend = 0,
+				layout_strategy = "vertical",
+				dynamic_preview_title = true,
+				path_display = filenameFirst,
+			},
+			pickers = {
+				live_grep = { only_sort_text = true, },
+				grep_string = { only_sort_text = true, },
+				colorscheme = { enable_preview = true, },
+				buffers = {
+					mappings = {
+						n = { ["<x>"] = "delete_buffer", },
+					},
+				},
+				git_files = {
+					hidden = true,
+					show_untracked = true,
+				},
+			},
+		})
+
+		local builtin = require('telescope.builtin')
+
+		vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = "[S]earch [f]iles" })
+		vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = "[S]earch Buffers []" })
+		vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = "[S]earch [r]esume" })
+		vim.keymap.set('n', '<leader>d', builtin.diagnostics, { desc = "[S]earch [d]iagnostics" })
+		vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, { desc = "[G]o to [d]efinitions" })
+		vim.keymap.set('n', '<leader>gr', builtin.lsp_references, { desc = "[G]o to [r]eferences" })
+		vim.keymap.set("n", "<leader>s?", builtin.help_tags, { desc = "[S]earch Help [?]" })
+		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [k]eymaps" })
+		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [b]uiltin" })
+		vim.keymap.set('n', '<leader>sg', function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end, { desc = "[S]earch [s]elect Telescope" })
+		vim.keymap.set('n', '<leader>sw', function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end, { desc = "[S]earch for [w]ord" })
+		vim.keymap.set('n', '<leader>sW', function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end, { desc = "[S]earch for [W]ORD" })
+	end
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,32 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-context",
-	},
-	opts = {
-		ensure_installed = {
-			"css",
-			"html",
-			"javascript",
-			"jsdoc",
-			"json",
-			"lua",
-			"markdown",
-			"python",
-			"scss",
-			"sql",
-			"rust",
-			"typescript",
-			"tsx",
-			"vim",
-			"vimdoc",
-		},
-		sync_install = false,
-		auto_install = true,
-		indent = {
-			enable = true,
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-
-		require("treesitter-context").setup({
-			enable = true,
-			max_lines = 0,
-			min_window_height = 0,
-			line_numbers = true,
-			multiline_threshold = 20,
-			trim_scope = "outer",
-			mode = "cursor",
-			separator = nil,
-			zindex = 20,
-			on_attach = nil,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"css",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"python",
+				"scss",
+				"sql",
+				"rust",
+				"typescript",
+				"tsx",
+				"vim",
+				"vimdoc",
+			},
+			sync_install = false,
+			auto_install = true,
+			indent = {
+				enable = true
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "markdown" },
+			},
 		})
-	end,
+	end
 }
