3127fef6c0a0345196d131a486ba4c57b0ba1d75
patnealcodes <hi@patneal.codes>
2026-05-05 07:36:24 -0400

    wez

---
+++ configs/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+
+config.color_scheme = 'catppuccin-mocha'
+config.font = wezterm.font('Agave Nerd Font Mono')
+config.font_size = 16
+config.enable_tab_bar = false
+config.enable_wayland = false
+config.window_decorations = "RESIZE"
+config.adjust_window_size_when_changing_font_size = false
+
+
+return config
